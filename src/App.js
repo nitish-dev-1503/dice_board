8992a44dcccbb94a8443d8cecf81a3acc50f928e
Nitish Sharma <nitish.sharma@thoughtworks.com>
2022-11-14 12:35:46 +0530

    Add DiceBoard logo at homepage header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import Tabs from './components/Tabs';
 
+import logo from './assets/diceboard.svg';
+
 function App() {
 
   return (
-    <div className="bg-neutral-200 text-primary_dark text-center">
+    <div className="bg-neutral-100 text-primary_dark text-center">
       <header className="py-20">
-        <p className='text-5xl font-bold'>Let's Play with Dice</p>
+        <img src={logo} className="h-30 my-4 m-auto" alt="logo" />
+        <p className='text-xl font-bold '>----- Let's Play with Dice -----</p>
       </header>
-      
+
       <Tabs />
 
-      <p className="text-sm my-4 ">All rights reserved | Nitish Sharma</p>
+      <p className="text-sm py-4 ">All rights reserved | Nitish Sharma</p>
 
     </div>
   );
