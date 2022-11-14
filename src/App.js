7726069fe5274e47fd2172c021791db9924c6b34
Nitish Sharma <nitish.sharma@thoughtworks.com>
2022-11-14 11:09:14 +0530

    Use Tabs component in App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import Player from './Player';
+import Tabs from './components/Tabs';
 
 function App() {
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Player />
+    <div className="bg-neutral-200 text-primary_dark text-center">
+      <header className="py-20">
+        <p className='text-5xl font-bold'>Let's Play with Dice</p>
       </header>
+      <Tabs />
+
+      <p className="text-sm my-4 ">All rights reserved | Nitish Sharma</p>
+
     </div>
   );
 }
